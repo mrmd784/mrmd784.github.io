de9d1302f77629bed1b9ce1c6877f739496a1671
Sachintha Moremada <96536774+mrmd784@users.noreply.github.com>
2025-04-24 12:27:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 async function showPlates(brand) {
   const res = await fetch('data.json');
   const data = await res.json();
-
   const plates = data[brand] || [];
 
   document.getElementById('brand-title').textContent = brand + " Plates";
-  document.getElementById('plate-list').innerHTML = plates.map(p => `<li>${p}</li>`).join('');
+  const list = document.getElementById('plate-list');
+  list.innerHTML = plates.map(p => `<li>${p}</li>`).join('');
+
   document.getElementById('result').classList.remove('hidden');
 }
